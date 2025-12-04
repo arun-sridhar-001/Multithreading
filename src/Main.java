73faf776124e9f4d5731acfcd34c1925d8408a65
arun-sridhar-001 <sridhararun947@gmail.com>
2025-12-04 21:21:29 +0530

    Extends Thread Class

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    private static void callStatic() {
+        System.out.println("Running static method");
+    }
+    public static void main(String[] args) {
+
+    }
+}
